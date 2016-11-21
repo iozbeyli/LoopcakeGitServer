643242a6a6b5f05551c8e3fae77d4959fbf0eb7c
mebegu <mehmetgurcay@ku.edu.tr>
2016-11-21 21:22:49 +0300

    hotfix4

--- controllers/createRepo.js
+++ controllers/createRepo.js
@@ -7,18 +7,13 @@ exports.create = function(req,res,next){
   var repo = req.body.repo;
   var usrEnv = path.resolve("../repos/users/"+user+"/"+repo+"/");
   var remote = path.resolve("../repos/remotes/"+repo+"/");
-  git.Repository.init(remote, isBare).then(function(repo){
-    clone = nodegit.Clone,
-    cred = nodegit.Cred;
-
-
-    clone.clone(remote, usrEnv,function(err){
-      if(err){
-        return console.log(err);
-      }
+  git.Repository.init(remote, isBare)
+    .then(function(repo){
+      git.Clone(remote, usrEnv)
+    .then(function(repo) {
       console.log("success: true, details: something happened.");
       return res.status(200).send({"success":true, "details": "something happened."});
-    })
+    });
 
 
   });
