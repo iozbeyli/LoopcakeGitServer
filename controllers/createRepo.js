541383be28b1d8fb473f5f94fd92ee026413131c
mebegu <mehmetgurcay@ku.edu.tr>
2016-11-21 16:41:04 +0300

    pRepo -> usrEnv

--- controllers/createRepo.js
+++ controllers/createRepo.js
@@ -6,7 +6,7 @@ exports.create = function(req,res,next){
   var user = req.user;
   var repo = req.repo;
   var usrEnv = path.resolve("../repos/users/"+user+"/"+repo+"/");
-  git.Repository.init(pRepo, isBare).then(function(repo){
+  git.Repository.init(usrEnv, isBare).then(function(repo){
     console.log("success: true, details: something happened.");
     return res.status(200).send({"success":true, "details": "something happened."});
   });
