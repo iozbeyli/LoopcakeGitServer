0627e574b9b58c9228f4dbcc4496cd458934bdcf
mebegu <mehmetgurcay@ku.edu.tr>
2016-11-21 21:05:35 +0300

    remote init

--- controllers/createRepo.js
+++ controllers/createRepo.js
@@ -1,14 +1,27 @@
 const git = require("nodegit");
 var path = require("path");
+var clone = git.Clone,
 
 exports.create = function(req,res,next){
   var isBare = 0;
   var user = req.body.user;
   var repo = req.body.repo;
   var usrEnv = path.resolve("../repos/users/"+user+"/"+repo+"/");
-  git.Repository.init(usrEnv, isBare).then(function(repo){
-    console.log("success: true, details: something happened.");
-    return res.status(200).send({"success":true, "details": "something happened."});
+  var remote = path.resolve("../repos/remotes/"+repo+"/");
+  git.Repository.init(remote, isBare).then(function(repo){
+    clone = nodegit.Clone,
+    cred = nodegit.Cred;
+
+
+    Clone.clone(remote, usrEnv)
+    .then(function(err)
+      if(err){
+        return console.log(err);
+      }
+      console.log("success: true, details: something happened.");
+      return res.status(200).send({"success":true, "details": "something happened."});
+    );
+
   });
 
 
