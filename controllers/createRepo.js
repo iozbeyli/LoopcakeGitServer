7681e8accad1392615a213b232534109333eda39
mebegu <mehmetgurcay@ku.edu.tr>
2016-11-23 17:56:09 +0300

    baring

--- controllers/createRepo.js
+++ controllers/createRepo.js
@@ -26,37 +26,7 @@ exports.create = function(req,res,next){
       console.log(err)
 
     git.Repository.init(remote, 1)
-    .then(function(repo) {
-      repository = repo;
-      return fse.writeFile(path.join(repository.workdir(), fileName), fileContent);
-    })
-    .then(function(){
-      return repository.refreshIndex();
-    })
-    .then(function(idx) {
-      index = idx;
-    })
-    .then(function() {
-      return index.addByPath(fileName);
-    })
-    .then(function() {
-      return index.write();
-    })
-    .then(function() {
-      return index.writeTree();
-    })
-    .then(function(oid) {
-      var author = git.Signature.now("Scott Chacon",
-        "schacon@gmail.com");
-      var committer = git.Signature.now("Scott A Chacon",
-        "scott@github.com");
-
-      // Since we're creating an inital commit, it has no parents. Note that unlike
-      // normal we don't get the head either, because there isn't one yet.
-      return repository.createCommit("HEAD", author, committer, "message", oid, []);
-    })
-    .done(function(commitId) {
-      console.log("New Commit: ", commitId);
+    .done(function() {
       fse.remove(usrEnv).then(function () {
         git.Clone(remote, usrEnv)
         .then(function(repo) {
